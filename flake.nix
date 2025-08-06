807069019940d15ef021ad75d3cdbf01aa5bed4e
midischwarz12 <midischwarz@proton.me>
2025-08-05 19:45:54 -0500

    feat: atomic-vim

--- flake.nix
+++ flake.nix
@@ -2,6 +2,11 @@
   inputs = {
     nixpkgs.url = "https://channels.nixos.org/nixos-25.05/nixexprs.tar.xz";
     nixpkgs-unstable.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+
+    atomic-vim = {
+      type = "git";
+      url = "https://codeberg.org/midischwarz12/atomic-vim";
+    };
   };
 
   outputs =
@@ -10,11 +15,14 @@
       nixpkgs,
       ...
     }:
+    let
+      system = "x86_64-linux";
+    in
     {
       nixosConfigurations.nixos-rohr = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
 
-        specialArgs = { inherit inputs self; };
+        specialArgs = { inherit inputs self system; };
 
         modules = [ ./configuration.nix ];
       };
